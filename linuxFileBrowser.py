7c03d0f45d8843dfcd6316c1b8cd904e2771f09b
qubies <tobias@renwick.tech>
2017-10-23 17:38:38 -0600

    adds toLower

--- linuxFileBrowser.py
+++ linuxFileBrowser.py
@@ -6,20 +6,22 @@ dirname1 = dirname
 for a in os.listdir(dirname1):
     print(a)
 
+def copy(path):
+
 while(True):
     try:
         i = list(map(str,input().strip().split()))
         if(i[0] == 'exit'):
             break
 
-        elif(i[0] == 'back'):
+        elif(i[0].lower() == 'back'):
             li = list(dirname.split('/'))
             del li[-1]
             dirname = '/'.join(li)
             if(dirname[-1] == ':'):
                 dirname = dirname + '/'
             print(dirname)
-        elif(i[0] == 'open'):
+        elif(i[0].lower() == 'open'):
             print(i[1])
             i[1] =  dirname + "/" + i[1]
           #  os.startfile(i[1])
@@ -29,7 +31,7 @@ while(True):
                 file.close()
             except IOError:
                 print("File couldn't be opened")
-
+        elif(i[0].lower() == '')
 
         else:
             dirname = dirname + '/' + i[0]
